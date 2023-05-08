fa80c8d08636bb1f3c02216789f6193ee8895e12
Santonion <125437179+Santonion@users.noreply.github.com>
2023-05-08 10:52:36 +0200

    Add files via upload

--- Fighting_game.py
+++ Fighting_game.py
@@ -1,90 +1,127 @@
 import pygame
-from pygame.locals import *
 
-# Startar Pygame
 pygame.init()
 
-# Gör ett fönster för spelet
-window_width = 1550
-window_height = 1000
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Ryo vs Ken")
+# Background image
+walkRight = pygame.image.load('DefaultP1.png')
+walkLeft = pygame.image.load('DefaultP1.png')
+bg = pygame.image.load("background.png")
+idle = [pygame.image.load('DefaultP1.png'), pygame.image.load('Default animation P1 1.png'), pygame.image.load('DefaultP1.png')]
+standcount = True
 
-# Karaktärernas utseende
-ryo_image = pygame.image.load('DefaultP1.png')
-ken_image = pygame.image.load('DeufaltP2.png')
+clock = pygame.time.Clock()
 
-# Backgrunden
-background_image = pygame.image.load('background.png')
+# Jump
+isJump = False
+jumpcount = 10
 
-# positionen när spelet börjar
-ryo_x = 125
-ryo_y = 500
-ken_x = 1100
-ken_y = 500
+# Window
+display_width = 1550
+display_height = 1000
+win = pygame.display.set_mode((display_width, display_height))
+
+# Title & Icon
+pygame.display.set_caption("Fighter Street")
+icon = pygame.image.load('DefaultP1.png')
+pygame.display.set_icon(icon)
+
+# Player 1
+player1_x = 50
+player1_y = 430
+player1_vel = 10
+
+# Player 1 Image
+player1_IMG = pygame.image.load('DefaultP1.png')
+
+# Player 2
+player2_x = 1200
+player2_y = 430
+player2_vel = 10
+
+# Player 2 Image
+player2_IMG = pygame.image.load('DeufaltP2.png')
+
+# Player Movement
+left = False
+right = False
+walkcount = 0
 
 
+def player1(x, y):
+    global standcount
+    global walkcount
+    win.blit(bg, (0, 0))
 
-# Lop för att röra spelarna
-while True:
-    # Handle user input
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            # Move Ryo using the arrow keys
-            if event.key == pygame.K_a:
-                ryo_x -= 10
-            elif event.key == pygame.K_d:
-                ryo_x += 10
-            elif event.key == pygame.K_w:
-                ryo_y -= 10
-            elif event.key == pygame.K_s:
-                ryo_y += 10
-            # Move Ken using the WASD keys
-            elif event.key == pygame.K_LEFT:
-                ken_x -= 10
-            elif event.key == pygame.K_RIGHT:
-                ken_x += 10
-            elif event.key == pygame.K_UP:
-                ken_y -= 10
-            elif event.key == pygame.K_DOWN:
-                ken_y += 10
+    if walkcount + 1 >= 9:
+        walkcount = 0
 
-    # Backgrund och spelarens bilder printas gång på gång
-    window.blit(background_image, (-120, 70))
-    window.blit(ryo_image, (ryo_x, ryo_y))
-    window.blit(ken_image, (ken_x, ken_y))
+    if standcount + 1 >= 9:
+        standcount = 0
+
+    if left:
+        win.blit(idle[walkcount // 3], (x, y))
+        walkcount += 1
+    elif right:
+        win.blit(idle[walkcount // 3], (x, y))
+        walkcount += 1
+    elif standcount:
+        p = 0
+        for frame in idle:
+            win.blit(idle[p], (x, y))
+            p += 1
+            if p >= 2:
+                p = 0
+                continue
+
     pygame.display.update()
 
-    # Gör en klocka för att skapa tidspress
-    clock = pygame.time.Clock()
 
-    # FPS
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Player movement
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a] and player1_x > player1_vel - 25:
+        player1_x -= player1_vel
+        left = True
+        right = False
+    elif keys[pygame.K_d] and player1_x < 1260:
+        player1_x += player1_vel
+        right = True
+        left = False
+    else:
+        right = False
+        left = False
+        walkcount = 0
+        standcount = True
+
+    if not isJump:
+        if keys[pygame.K_SPACE]:
+            isJump = True
+            left = False
+            right = False
+
+    else:
+        if jumpcount >= -10:
+            neg = 1
+            if jumpcount < 0:
+                neg = -1
+            player1_y -= (jumpcount ** 2) * 0.5 * neg
+            jumpcount -= 1
+        else:
+            isJump = False
+            jumpcount = 10
+
+    player1(player1_x, player1_y)
+    pygame.display.update()
+
+    # Limit FPS to 60
     clock.tick(60)
 
-    # Mängden liv
-    ryo_health = 100
-    ken_health = 100
-
-    # Slut på spelet
-    if ryo_health <= 0:
-        print("Ken wins!")
-        game_over = True
-    elif ken_health <= 0:
-        print("Ryo wins!")
-        game_over = True
-
-    ryo_rect = Rect(200, 500, 50, 50)
-    ken_rect = Rect(200, 0, 50, 50)
-
-    # Fixar så att karaktärerna skadar varandra när dom rörsvid
-    ryo_rect = ryo_image.get_rect()
-    ryo_rect.x = ryo_x
-    ryo_rect.y = ryo_y
-    ken_rect = ken_image.get_rect()
-    ken_rect.x = ken_x
-    ken_rect.y = ken_y
-    if ryo_rect.colliderect(ken_rect):
-        ryo_health -= 10
-        ken_health -= 10
+# Quit pygame
+pygame.quit()
